cb6f8fd54e3f28da4fe7955d19fffcb96000f20f
Herdejurgen <98052580+Herdejurgen@users.noreply.github.com>
2022-09-19 12:24:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   </head>
   
   <body>
-<!--     <?php require_once(“header.php”); ?>  -->
+    <?php include(“header.php”); ?>
     <div class="container-fluid">
               
       <div class="position-absolute top start">
