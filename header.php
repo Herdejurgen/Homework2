d303c39bb816e62254c729d63e4af024bb4d9e0a
Herdejurgen <98052580+Herdejurgen@users.noreply.github.com>
2022-09-19 12:40:23 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,16 +8,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="http://page1.herdejurgen.oucreate.com">Page 1</a>
+            <a class="nav-link" href='page1.php'>Page 1</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://page2.herdejurgen.oucreate.com">Page 2</a>
+            <a class="nav-link" href='page2.php'>Page 2</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://page3.herdejurgen.oucreate.com">Page 3</a>
+            <a class="nav-link" href='page3.php'>Page 3</a>
           </li>            
           <li class="nav-item">
-            <a class="nav-link" href='http://page4.herdejurgen.oucreate.com'>Page 4</a>
+            <a class="nav-link" href='page4.php'>Page 4</a>
           </li>
         </ul>
       </div>
