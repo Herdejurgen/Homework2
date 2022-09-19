537d8dd9848f848b0adf470e24d003390aa779e9
Herdejurgen <98052580+Herdejurgen@users.noreply.github.com>
2022-09-19 12:10:55 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -8,30 +8,7 @@
   </head>
   
   <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="http://page1.herdejurgen.oucreate.com">Page 1</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://page2.herdejurgen.oucreate.com">Page 2</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://page3.herdejurgen.oucreate.com">Page 3</a>
-            </li>            
-            <li class="nav-item">
-              <a class="nav-link" href='http://page4.herdejurgen.oucreate.com'>Page 4</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php require_once(“header.php”); ?> 
     <div class="container-fluid">
               
       <div class="position-absolute top start">
