58f8356263b897a02751514f368a6e5915b5210b
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-09-28 13:40:11 -0500

    take out color input

--- page4.php
+++ page4.php
@@ -21,7 +21,7 @@
         $v3 = PickAColor();
       ?>
       <div class="row-2">
-      <h1 style = "color:rgb(<?php PickAColor(); ?>, <?php PickAColor(); ?>, <?php PickAColor(); ?>);">GOOOOOD Morning</h1>
+      <h1 style = "color:rgb( , ,);">GOOOOOD Morning</h1>
         <div class="spinner-border" style="color:rgb(<?PickAColor();?>,<?PickAColor();?>,<?PickAColor();?>)" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
