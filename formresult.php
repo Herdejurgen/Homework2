0281adfa805d8607fd855324215797025d051210
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-10-02 15:45:07 -0500

    clean up result

--- formresult.php
+++ formresult.php
@@ -10,9 +10,9 @@
     <?php include'header.php'; ?>
     <div class="container-fluid">
     <h2>These are the numbers you input:</h2>
-    <h2> <?php echo $_GET["num1"]; ?></h2><br>
-    <h2> <?php echo $_GET["num2"]; ?></h2><br>
-    <h2> <?php echo $_GET["num3"]; ?></h2><br>
+    <h2> <?php echo $_GET["num1"]; ?></h2>
+    <h2> <?php echo $_GET["num2"]; ?></h2>
+    <h2> <?php echo $_GET["num3"]; ?></h2>
     </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
