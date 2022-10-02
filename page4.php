76cebc5f8b853cfa88b738528843ccaa8e9f5b64
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-10-02 15:16:59 -0500

    clean up

--- page4.php
+++ page4.php
@@ -11,7 +11,6 @@
     <?php include'header.php'; ?>
     <div class="container-fluid">
       <div class="row-2">      
-      <h1>GOOOOOD Morning</h1>
       <?php        
         $v = rand(0,255);
         $w = rand(0,255);
@@ -20,34 +19,7 @@
         echo "<h2>" . $v . "</h2>";
         echo "<h2>" . $w . "</h2>";
         echo "<h2>" . $x . "</h2>";
-      ?>      
-        <div class="spinner-border text-primary" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-secondary" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-success" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-danger" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-warning" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-info" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-light" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-dark" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-      </div>
-      <h3>But I wish I could make them purple...</h3>
-      <h4>Purple is my favorite color!</h4>
+      ?>
     </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
