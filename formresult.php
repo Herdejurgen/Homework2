621d54ac9880aacf74f4bcc2a52ecc200542973e
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-10-02 15:37:26 -0500

    post to get

--- formresult.php
+++ formresult.php
@@ -11,9 +11,9 @@
     <div class="container-fluid">
     <h2>These are the numbers you input:</h2>
     <?php
-        echo $_POST["num1"];
-        echo $_POST["num2"];
-        echo $_POST["num3"];
+        echo $_GET["num1"];
+        echo $_GET["num2"];
+        echo $_GET["num3"];
         ?>
     </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
